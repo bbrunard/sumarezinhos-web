84fb8a39099fdba874907f1f2a7b30478d5640fa
Bruna <bruna0823@hotmail.com>
2026-06-14 19:59:21 -0300

    Ajuste do interceptor

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -3,9 +3,20 @@ import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = inject(AuthService).getToken();
-  if (token) {
-    req = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
+
+  if (req.url.includes('api.cloudinary.com')) {
+    return next(req);
   }
+
+  const token = inject(AuthService).getToken();
+
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+
   return next(req);
 };
